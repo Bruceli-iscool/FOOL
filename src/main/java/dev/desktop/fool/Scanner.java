0183e760bec344daf2ded4a5426f24bf6b59a3e8
Bruce Li <isanoob1897@outlook.com>
2024-03-10 19:34:36 -0700

    add

--- src/main/java/dev/desktop/fool/Scanner.java
+++ src/main/java/dev/desktop/fool/Scanner.java
@@ -8,6 +8,85 @@ import java.util.Map;
 import static dev.desktop.fool.TokenType.*;
 
 class Scanner {
+    /* Scan for tokens*/
     private final String source;
-    private final 
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source) {
+        this.source = source;
+    }
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+    private void scanToken() {
+        // Scan a single token
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            // multi character handling
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            // slash handling
+            case '/':
+                if (match('/')) {
+                    // comments
+                    while (peek() != '\n' && !isAtEnd()); advance();
+                }
+            // error handling
+            default:
+                Fool.error(line, "Unexpected char.");
+                break;
+        }
+    }
+    /* Helper functions*/
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+    }
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+    private char advance() {
+        return source.charAt(current++);
+    }
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+    private void addToken(TokenType type, Object literal){
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
 }
