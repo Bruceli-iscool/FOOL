8ae49e22d4aeb71b3431d7681413c965983cdb6f
Bruce Li <isanoob1897@outlook.com>
2024-03-12 19:51:31 -0700

    add

--- src/main/java/dev/desktop/tool/GenerateAST.java
+++ src/main/java/dev/desktop/tool/GenerateAST.java
@@ -12,11 +12,26 @@ public class GenerateAST {
             System.exit(64);
         }
         String outputDir = args[0];
-        defineAST(outputDir, "Expr", Arrays.asList(
+        defineAst(outputDir, "Expr", Arrays.asList(
                 "Binary   : Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
                 "Literal  : Object value",
                 "Unary    : Token operator, Expr right"
         ));
+        private static void defineAst(
+                String outputDir, String baseName, List<String> types)
+      throws IOException {
+            String path = outputDir + "/" + baseName + ".java";
+            PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+            writer.println("dev.desktop.fool;");
+            writer.println();
+            writer.println("import java.util.List;");
+            writer.println();
+            writer.println("abstract class " + baseName + " {");
+
+            writer.println("}");
+            writer.close();
+        }
     }
 }
