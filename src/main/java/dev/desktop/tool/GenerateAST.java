5c1251203e5f017fc488a47c1e9c4443f06bab55
Bruce Li <isanoob1897@outlook.com>
2024-03-12 19:44:36 -0700

    add

---
+++ src/main/java/dev/desktop/tool/GenerateAST.java
@@ -0,0 +1,22 @@
+package dev.desktop.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAST {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+        defineAST(outputDir, "Expr", Arrays.asList(
+                "Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator, Expr right"
+        ));
+    }
+}
